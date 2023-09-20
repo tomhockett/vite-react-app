430a6c0844614edb4a87eef0cde5e9e5defc745a
Tom Hockett <tom.hockett@gmail.com>
2023-09-20 13:19:21 -0500

    rename and refactor

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
-import SubHeading from './SomeMath.jsx'
+import SubHeading from './SubHeading.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
