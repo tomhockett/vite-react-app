786848fba00cb95198bc966e35abab75342bdd60
Tom Hockett <tom.hockett@gmail.com>
2023-09-20 13:15:58 -0500

    init repo

---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;Hello!, and Welcome!&quot;</h1>
+}
+export default Greeting;
+
---
+++ src/SomeMath.jsx
@@ -0,0 +1,7 @@
+function SubHeading() {
+  return (<h3>
+    Here is a subheading, of some text I think is cool
+    </h3>);
+}
+
+export default SubHeading;
