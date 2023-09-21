43316069d9f37857ae75a697fc72326d250b5168
Tom Hockett <tom.hockett@gmail.com>
2023-09-20 22:50:52 -0500

    add todo list component

---
+++ src/TodoList.jsx
@@ -0,0 +1,29 @@
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr&apos;s Todos for {formatDate(today)}</h1>
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo"
+      />
+      <ul style={{
+        backgroundColor: 'green',
+        color: 'white'
+      }}>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import SubHeading from './SubHeading.jsx'
+import TodoList from './TodoList.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Greeting />
     <SubHeading />
+    <TodoList />
   </React.StrictMode>,
 )
